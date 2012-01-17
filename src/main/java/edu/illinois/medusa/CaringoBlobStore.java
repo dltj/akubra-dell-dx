88ebf855263f136bb112ab824e7ce5a6fcfae87b
Howard Ding <hding2@illinois.edu>
2012-01-17 16:36:10 -0600

    Add javadoc

--- src/main/java/edu/illinois/medusa/CaringoBlobStore.java
+++ src/main/java/edu/illinois/medusa/CaringoBlobStore.java
@@ -12,27 +12,47 @@ import javax.print.DocFlavor;
 import javax.transaction.Transaction;
 
 /**
- * Created by IntelliJ IDEA.
- * User: hading
- * Date: 7/14/11
- * Time: 12:58 PM
- * To change this template use File | Settings | File Templates.
+ * Implement Akubra AbstractBlobStore for Caringo storage server.
+ *
+ * @author Howard Ding - hding2@illinois.edu
  */
 public class CaringoBlobStore extends AbstractBlobStore {
 
+    /**
+     * Configuration for connecting to Caringo server
+     */
     protected CaringoConfigConnection connectionConfig;
+    /**
+     * Configuration for authenticating to Caringo server.
+     */
     protected CaringoConfigAuthentication authenticationConfig;
-
+    /**
+     * Stream manager for the BlobStore
+     */
     protected StreamManager streamManager;
 
+    /**
+     * The bucket used on the storage server.
+     * @return Bucket name used on storage server.
+     */
     public String getBucketName() {
         return connectionConfig.caringoBucket;
     }
 
+    /**
+     * The stream manager for the BlobStore.
+     * @return  StreamManager for this BlobStore
+     */
     public StreamManager getStreamManager() {
         return streamManager;
     }
 
+    /**
+     * Constructor with ID and configuration for connection and authentication
+     * @param storeId ID for this store
+     * @param connectionConfig Configuration for connection to storage server
+     * @param authenticationConfig Configuration for authentication to storage server. May be null if authentication is unused.
+     */
     protected CaringoBlobStore(URI storeId, CaringoConfigConnection connectionConfig, CaringoConfigAuthentication authenticationConfig) {
         super (storeId);
         this.connectionConfig = connectionConfig;
@@ -40,10 +60,22 @@ public class CaringoBlobStore extends AbstractBlobStore {
         this.streamManager = new StreamManager();
     }
 
+    /**
+     * Constructor with ID and configuration for connection. Authentication will not be used.
+     * @param storeId ID for this store
+     * @param connectionConfig Configuration for connection to storage server
+     */
     protected CaringoBlobStore(URI storeId, CaringoConfigConnection connectionConfig) {
         this(storeId, connectionConfig, null);
     }
 
+    /**
+     * Open a new connection to this store.
+     * @param tx For this BlobStore this must be null - transactions are unsupported.
+     * @param hints For this BlobStore this is ignored.
+     * @return  A new connection to this BlobStore
+     * @throws IOException If the tx argument is not null or there is an error opening the connection.
+     */
     public CaringoBlobStoreConnection openConnection(Transaction tx, Map<String, String> hints) throws IOException {
         if (tx != null) {
             throw new UnsupportedOperationException();
@@ -51,10 +83,20 @@ public class CaringoBlobStore extends AbstractBlobStore {
         return new CaringoBlobStoreConnection(this, streamManager);
     }
 
+    /**
+     * Open a new connection to this store
+     * @return A new connection to this BlobStore
+     * @throws IOException If there is an error opening the connection.
+     */
     public CaringoBlobStoreConnection openConnection() throws IOException {
         return this.openConnection(null, null);
     }
 
+    /**
+     * Get a new SDK ScspClient to Caringo storage.
+     * @return A new SDK client to Caringo storage
+     * @throws IOException If there is an error getting a client
+     */
     public ScspClient getCaringoClient() throws IOException {
         String[] hosts = new String[1];
         hosts[0] = connectionConfig.serverURL;
