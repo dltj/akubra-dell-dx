8e2d2872a57172e9d6676fcad5cfcb3e4ec6845e
Howard Ding <hding2@illinois.edu>
2012-01-17 15:26:35 -0600

    Add some javadoc

--- src/main/java/edu/illinois/medusa/CaringoAbstractResponse.java
+++ src/main/java/edu/illinois/medusa/CaringoAbstractResponse.java
@@ -3,37 +3,63 @@ package edu.illinois.medusa;
 import com.caringo.client.ScspResponse;
 
 /**
- * Created by IntelliJ IDEA.
- * User: hading
- * Date: 7/18/11
- * Time: 11:29 AM
- * To change this template use File | Settings | File Templates.
+ * Wraps the ScspResponse returned from an interaction with a Caringo server, adding some convenience methods
+ * along with access to the ScspResponse itself if needed.
+ *
+ * @author Howard Ding <hding2@illinois.edu>
  */
+
 public abstract class CaringoAbstractResponse {
 
-
+    /**
+     * The response from the Caringo server
+     */
     protected ScspResponse response;
 
+    /**
+     * Construct from an ScspResponse
+     * @param response
+     */
     protected CaringoAbstractResponse(ScspResponse response) {
         this.response = response;
     }
 
+    /**
+     * Get the HTTP status of the response
+     * @return The integer status code of the Caringo response
+     */
     public int status() {
         return response.getHttpStatusCode();
     }
 
+    /**
+     * Get whether the response returned ok (200)
+     * @return Whether the status was ok (200).
+     */
     public boolean ok() {
         return this.status() == 200;
     }
 
+    /**
+     * Get whether the response returned not found (404)
+     * @return Whether the status was not found (404)
+     */
     public boolean notFound() {
         return this.status() == 404;
     }
 
+    /**
+     * Get whether the response returned created (201)
+     * @return Whether the status was created (201)
+     */
     public boolean created() {
         return this.status() == 201;
     }
 
+    /**
+     * Get the wrapped response
+     * @return The wrapped ScspResponse
+     */
     public ScspResponse scspResponse() {
         return this.response;
     }
