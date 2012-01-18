62009710d2352110efd1f2b9f6bcf2394a14e421
Howard Ding <hding2@illinois.edu>
2012-01-18 11:43:21 -0600

    Javadoc

--- src/main/java/edu/illinois/medusa/CaringoIdMapper.java
+++ src/main/java/edu/illinois/medusa/CaringoIdMapper.java
@@ -5,14 +5,18 @@ import org.akubraproject.map.IdMapper;
 import java.net.URI;
 
 /**
- * Created by IntelliJ IDEA.
- * User: hading
- * Date: 8/3/11
- * Time: 1:11 PM
- * To change this template use File | Settings | File Templates.
+ * This is a completely trivial implementation of the Akubra IdMapper, which is to say that it makes
+ * no changes to the IDs. This is sufficient for use with Fedora, which (as far as I can tell) uses no
+ * IDs that need any quoting. Moreover at this point I feel that maintaining the Fedora IDs as is is more
+ * important than being totally general for Caringo. Both are achievable, but perhaps not trivially (e.g.
+ * I think URLencoding/decoding would make all strings okay for Caringo, but it would quote things that
+ * I wouldn't necessarily want quoted).
+ *
+ * At some point we may revisit this class and make it do both things, but that time is not right now.
+ *
+ * @author Howard Ding - hding2@illinois.edu
  */
 
-/* for now this is a completely trivial implementation */
 public class CaringoIdMapper implements IdMapper {
 
     public URI getExternalId(URI internalId) throws NullPointerException {
