3eb91055231d1b25099f5ddda55232ca020a7c05
Howard Ding <hding2@illinois.edu>
2012-03-30 10:54:13 -0500

    Update comments

---
+++ src/main/java/edu/illinois/medusa/FedoraObjectBlobStore.java
@@ -0,0 +1,11 @@
+package edu.illinois.medusa;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hading
+ * Date: 3/30/12
+ * Time: 10:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FedoraObjectBlobStore {
+}
