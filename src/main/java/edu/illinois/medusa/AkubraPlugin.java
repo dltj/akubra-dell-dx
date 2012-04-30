28696a8a5f043cb1fc2295f9de5d5197ee01a8ef
Howard Ding <hding2@illinois.edu>
2012-04-30 11:30:36 -0500

    Documentation.

--- src/main/java/edu/illinois/medusa/AkubraPlugin.java
+++ src/main/java/edu/illinois/medusa/AkubraPlugin.java
@@ -1,19 +1,34 @@
 package edu.illinois.medusa;
 
 /**
- * Created with IntelliJ IDEA.
- * User: hading
- * Date: 4/26/12
- * Time: 11:08 AM
- * To change this template use File | Settings | File Templates.
+ * Global information about the plugin. Used to populate some Dell requested headers on objects.
+ *
+ * @author Howard Ding - hding2@illinois.edu
  */
 public class AkubraPlugin {
+
+    /**
+     * Major version of the plugin
+     */
     public static final String MAJOR_VERSION = "1";
+    /**
+     * Minor version of the plugin
+     */
     public static final String MINOR_VERSION = "0";
+    /**
+     * Revision number of the plugin
+     */
     public static final String REVISION = "0";
 
+    /**
+     * String describing the plugin as the actual creator of an object in storage
+     */
     public static final String dellCreator = "Akubra";
 
+    /**
+     *
+     * @return - full string concatenating version information, formatted per Dell guidelines.
+     */
     public static String dellCreatorVersion() {
         return MAJOR_VERSION + ";" + MINOR_VERSION + ";" + REVISION;
     }
